ef3b39d68855d4888255b0887ea6a43af3e37b3b
SteveBanh02 <steve.banh02@gmail.com>
2025-10-05 01:25:16 -0400

    Update Fibonacci.java added more clarity

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -14,27 +14,25 @@ public class Fibonacci {
     public static int recursiveFibonacci(int n){
 
         /*f(n) = f(n-1) + f(n-2)
-        * if n = 5
+        * if n = 6
         * the program starts "going down" like a tree
+        * f(6) = f(5) + f(4)
         * f(5) = f(4) + f(3)
         * f(4) = f(3) + f(2)
         * f(3) = f(2) + f(1)
-        * f(2) = f(1) + f(0) --> f(2) = 1 + 1 = 2
-        * f(1) = f(0)
+        * f(2) = f(1) --> f(2) = 1
         *
         * f(1) and f(0) is both equal to 1 because of the base case
         *
         * once it finds what it needs, it starts "going back up"
-        * f(3) = f(2) + f(1) --> f(3) = 2 + 1 = 3
-        * f(4) = f(3) + f(2) --> f(4) = 3 + 2 = 5
-        * f(5) = f(4) + f(3) --> f(5) = 5 + 3 = 8
-        *
+        * f(3) = f(2) + f(1) --> f(3) = 1 + 1 = 2
+        * f(4) = f(3) + f(2) --> f(4) = 2 + 1 = 3
+        * f(5) = f(4) + f(3) --> f(5) = 3 + 2 = 5
+        * f(6) = f(5) + f(4) --> f(6) = 5 + 3 = 8
         * therefore, the program returns 8
         * */
-        if(n == 0){
-            return 0;//base case since f(0) starts at 0
-        }else if (n == 1 || n == 2){
-            return 1;//base case since f(1) and f(2) are both 1
+        if(n <= 1){
+            return n; //Returns whatever n is at the current moment
         }
         return recursiveFibonacci(n-1) + recursiveFibonacci(n-2);
     }
